6691308c14c380d5c1caa7ee1cebe5c979b45801
Nelson Macias <nelson.macias99@gmail.com>
2024-11-09 13:41:36 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# El Laberinto
+________
+
+Se necesita de la version 22 o superior del jdk para funcionar
---
+++ src/Main.java
@@ -0,0 +1,62 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        char[][] maze = {
+                {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+                {'#', 'S', ' ', ' ', '#', '#', ' ', ' ', '#', '#'},
+                {'#', '#', '#', ' ', '#', '#', '#', ' ', ' ', 'E'},
+                {'#', '#', ' ', ' ', ' ', '#', '#', ' ', '#', '#'},
+                {'#', '#', ' ', '#', '#', ' ', ' ', ' ', '#', '#'},
+                {'#', '#', ' ', '#', '#', ' ', '#', ' ', '#', '#'},
+                {'#', ' ', ' ', ' ', '#', ' ', '#', '#', '#', '#'},
+                {'#', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', '#'},
+                {'#', '#', '#', ' ', ' ', ' ', '#', '#', ' ', '#'},
+                {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+        };
+
+        boolean solved = solveMaze(maze, 1, 1);
+
+        if (solved) {
+            System.out.println("Laberinto solucionado!");
+        } else {
+            System.out.println("No se pudo solucionar");
+        }
+
+        showMaze(maze);
+    }
+
+    public static void showMaze(char[][] maze) {
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[i].length; j++) {
+                System.out.print(" " + maze[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static boolean solveMaze(char[][] maze, int row, int col) {
+        // Comprobar si encontramos la salida
+        if (maze[row][col] == 'E') {
+            return true;
+        }
+
+        // Comprobar si estamos en una posición válida para movernos
+        if (maze[row][col] != ' ' && maze[row][col] != 'S') {
+            return false;
+        }
+
+        // Marcar la celda actual como parte del camino de la solución
+        maze[row][col] = '.';
+
+        // Intentar moverse en cada dirección: abajo, derecha, arriba, izquierda
+        if (solveMaze(maze, row, col + 1)) return true; // Derecha
+        if (solveMaze(maze, row + 1, col)) return true; // Abajo
+        if (solveMaze(maze, row, col - 1)) return true; // Izquierda
+        if (solveMaze(maze, row - 1, col)) return true; // Arriba
+
+        // Si ninguna dirección funciona, retroceder (desmarcar la celda)
+        maze[row][col] = ' ';
+        return false;
+    }
+}
